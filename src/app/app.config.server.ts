9cfa7982eb25432d535f3b32ccc7219db979e876
Esakkimuthu.A <mesakki834@gmail.com>
2025-05-28 23:05:25 +0530

    changes

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,12 +3,8 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
-// import { FlexLayoutModule } from '@angular/flex-layout';
 
 const serverConfig: ApplicationConfig = {
-  // imports :[
-  //   FlexLayoutModule
-  // ],
   providers: [
     provideServerRendering(),
     provideServerRouting(serverRoutes)
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,16 @@
-// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
-
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-// Translation loader factory
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-// Bootstrap with merged config
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
